3d812334448d3cf12db6dd06135ff5db51fd951f
Inés Álvarez <inalca@floridauniversitaria.es>
2023-02-16 09:28:28 +0100

    Tercer commit

--- atotal.sh
+++ atotal.sh
@@ -3,7 +3,7 @@
 echo "Introduce el nombre de una ciudad: "
 read $ciudad
 
-if [[ -n $(grep -i "$ciudad" ciudades.txt) ]]; then
+if [[ -n $(grep -i "$ciudad") ]]; then
  $total=$(cat consumo.txt | grep -i "$ciudad" | awk '{total += $2} END {print total}')
  echo "El total de consumo para $ciudad es $total"
 else
