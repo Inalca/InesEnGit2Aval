c6be88aae7331a43b2f1ceb197146c077da5baa1
Inés Álvarez Calle <122776241+Inalca@users.noreply.github.com>
2023-02-16 10:17:50 +0100

    Add files via upload

---
+++ atotal.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Introduce el nombre de una ciudad: "
+read $ciudad
+
+if [[ -n $(grep -i "$ciudad") ]]; then
+ $total=$(cat consumo.txt | grep -i "$ciudad" | awk '{total += $2} END {print total}')
+ echo "El total de consumo para $ciudad es $total"
+else
+ echo "La ciudad introducida no existe, inténtalo de nuevo"
+fi
+
+exit
